9ac14fda0f906f77afc20070dd708aa522c44232
Juri Knauth <knauth.juri@gmail.com>
2017-11-02 20:33:49 +0100

    test

--- jkscript.js
+++ jkscript.js
@@ -1,9 +1,13 @@
 		$(function(){
 			var path = window.location.href;
 			$('a').each(function(){
-				var href = $(this).prop('onclick');
+				var href = $(this).prop('id');
 				if (path.toLowerCase().indexOf(href).toLowerCase() >= 0) {
-					$(this).attr('id', ('Blog'));
+
+				}
+				else
+				{
+					$(this).attr('id', ('Default'));					
 				}
 			});
 		});
