312af22651b70936362503a6c0d721a02e63fa89
Juri Knauth <knauth.juri@gmail.com>
2017-11-03 19:33:08 +0100

    test

--- jkscript.js
+++ jkscript.js
@@ -2,7 +2,9 @@
 		
 		function buttonColor(){
 				var href = $(this).prop('href');
-				if (path.toLowerCase().indexOf(href) >= 0) {
+				var e = document.getElementById("monkey");
+				e.id = "ape";
+				if (path.toLowerCase().indexOf(e.id) >= 0) {
 					$(this).attr('id', ('Default'));
 				}
 		}
@@ -89,7 +91,7 @@
         function myFunction(i) {
             document.getElementById(i).classList.toggle("show");
         }
-
+		
         // Close the dropdown if the user clicks outside of it
         window.onclick = function (e) {
             if (!e.target.matches('.dropbtn')) {
@@ -99,4 +101,29 @@
                 }
             }
         }	
+		
+		function buttonEvent(event) {
+			var x = event.which || event.keyCode;
+			if (x == 27) {
+				document.getElementById('myModal').style.display = "none";
+			}
+			else if (x == 37) {
+				plusSlides(-1);
+			}
+			else if (x == 39) {
+				plusSlides(1);
+			}
+			else if (x == 66 || x == 49) {
+				window.location.href='/;
+			}
+			else if (x == 80 || x == 50) {
+				window.location.href='http://www.juriknauth.com/Blog.html';
+			}
+			else if (x == 67 || x == 51) {
+				window.location.href='http://www.juriknauth.com/Projects.html';
+			}
+			else if (x == 74 || x == 52 || x == 72) {
+				window.location.href='http://www.juriknauth.com/Contact.html';
+			}
+		}
 		
