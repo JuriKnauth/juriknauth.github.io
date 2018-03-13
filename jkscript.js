5983ade07e419966559ebdcde419b6f9eaf0dcb1
Juri Knauth <knauth.juri@gmail.com>
2018-03-13 23:51:31 +0100

    STOMT

--- jkscript.js
+++ jkscript.js
@@ -90,3 +90,19 @@ window.onclick = function(e) {
     }
   }
 };
+
+// Include the STOMT JavaScript SDK
+(function(w, d, n, r, t, s) {
+  w.Stomt = w.Stomt || [];
+  t = d.createElement(n);
+  s = d.getElementsByTagName(n)[0];
+  t.async = 1;
+  t.src = r;
+  s.parentNode.insertBefore(t, s);
+})(window, document, "script", "https://www.stomt.com/widget.js");
+
+// Adjust the 'APP_ID' to your application id
+// you can find it here: https://www.stomt.com/YOUR_PAGE/apps
+Stomt.push(["addTab", { appId: "APP_ID" }]);
+Stomt.push(["addFeed", { appId: "APP_ID" }]);
+Stomt.push(["addCreate", { appId: "APP_ID" }]);
