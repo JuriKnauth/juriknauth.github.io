4da337d59c3d8846a75bfa9edc0baf11757d9b96
Juri Knauth <knauth.juri@gmail.com>
2019-06-22 00:05:15 +0200

    test

---
+++ entries/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>LoadMore</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+        <script type="text/javascript" src="jquery.js"></script>
+        <script type="text/javascript">
+        
+
+
+        </script>
+        <style type="text/css">
+
+        .blog-post{ 
+            border-bottom: solid 3px black;
+            margin-bottom: 20px;
+        }
+
+        .blog.post h1{
+           font-size: 40px;
+        }
+
+        .blog.post p{
+           font-size: 30px;
+        }
+
+        </style>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        
+        <h1>Heading 1</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam, provident? Repudiandae, nulla minima optio et dignissimos atque commodi repellendus adipisci dolorem beatae reiciendis laboriosam ullam hic cum animi temporibus autem.</p>
+
+        <script src="" async defer></script>
+    </body>
+</html>
