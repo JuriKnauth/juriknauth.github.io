1eddd676a3567754526b96dbf84c9359740a7207
Juri Knauth <knauth.juri@gmail.com>
2019-06-23 09:38:40 +0200

    focustimer

--- jkscript.js
+++ jkscript.js
@@ -1,186 +1,184 @@
 // Include the STOMT JavaScript SDK
-  (function(w, d, n, r, t, s){
-    w.Stomt = w.Stomt||[];
+(function(w, d, n, r, t, s) {
+    w.Stomt = w.Stomt || [];
     t = d.createElement(n);
     s = d.getElementsByTagName(n)[0];
-    t.async=1;
-    t.src=r;
-    s.parentNode.insertBefore(t,s);
-  })(window, document, 'script', 'https://www.stomt.com/widget.js');
- 
-	  Stomt.push(['addTab', {appId: 'zJAwvWmlssF2Z8YCTAKehPgAa'}]);
-	  Stomt.push(['addFeed', {appId: 'zJAwvWmlssF2Z8YCTAKehPgAa'}]);
-	  Stomt.push(['addCreate', {appId: 'zJAwvWmlssF2Z8YCTAKehPgAa'}]);
+    t.async = 1;
+    t.src = r;
+    s.parentNode.insertBefore(t, s);
+})(window, document, 'script', 'https://www.stomt.com/widget.js');
+
+Stomt.push(['addTab', { appId: 'zJAwvWmlssF2Z8YCTAKehPgAa' }]);
+Stomt.push(['addFeed', { appId: 'zJAwvWmlssF2Z8YCTAKehPgAa' }]);
+Stomt.push(['addCreate', { appId: 'zJAwvWmlssF2Z8YCTAKehPgAa' }]);
 
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
 
 function openTab(tabName, elmnt, color) {
-  var i, tabcontent, tablinks, articlebody;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].style.backgroundColor = "";
-    tablinks[i].style.color = "#FFFFFF";
-  }
-  articlebody = document.getElementsByClassName("articlebody");
-  for (i = 0; i < articlebody.length; i++) {
-    articlebody[i].style.backgroundColor = "";
-  }
-  document.getElementById(tabName).style.display = "block";
-  elmnt.style.backgroundColor = color;
-  elmnt.style.color = "#000000";
+    var i, tabcontent, tablinks, articlebody;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+        tablinks[i].style.color = "#FFFFFF";
+    }
+    articlebody = document.getElementsByClassName("articlebody");
+    for (i = 0; i < articlebody.length; i++) {
+        articlebody[i].style.backgroundColor = "";
+    }
+    document.getElementById(tabName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+    elmnt.style.color = "#000000";
 }
 
 /* lazyload.js (c) Lorenzo Giuliani
-	 * MIT License (http://www.opensource.org/licenses/mit-license.html)
-	 *
-	 * expects a list of:
-	 * `<img src="blank.gif" data-src="my_image.png" width="600" height="400" class="lazy">`
-	 */
-	 $(function() {
-	  var $q = function(q, res){
-	        if (document.querySelectorAll) {
-	          res = document.querySelectorAll(q);
-	        } else {
-	          var d=document
-	            , a=d.styleSheets[0] || d.createStyleSheet();
-	          a.addRule(q,'f:b');
-	          for(var l=d.all,b=0,c=[],f=l.length;b<f;b++)
-	            l[b].currentStyle.f && c.push(l[b]);
+ * MIT License (http://www.opensource.org/licenses/mit-license.html)
+ *
+ * expects a list of:
+ * `<img src="blank.gif" data-src="my_image.png" width="600" height="400" class="lazy">`
+ */
+$(function() {
+    var $q = function(q, res) {
+            if (document.querySelectorAll) {
+                res = document.querySelectorAll(q);
+            } else {
+                var d = document,
+                    a = d.styleSheets[0] || d.createStyleSheet();
+                a.addRule(q, 'f:b');
+                for (var l = d.all, b = 0, c = [], f = l.length; b < f; b++)
+                    l[b].currentStyle.f && c.push(l[b]);
 
-	          a.removeRule(0);
-	          res = c;
-	        }
-	        return res;
-	      }
-	    , addEventListener = function(evt, fn){
-	        window.addEventListener
-	          ? this.addEventListener(evt, fn, false)
-	          : (window.attachEvent)
-	            ? this.attachEvent('on' + evt, fn)
-	            : this['on' + evt] = fn;
-	      }
-	    , _has = function(obj, key) {
-	        return Object.prototype.hasOwnProperty.call(obj, key);
-	      }
-	    ;
+                a.removeRule(0);
+                res = c;
+            }
+            return res;
+        },
+        addEventListener = function(evt, fn) {
+            window.addEventListener ?
+                this.addEventListener(evt, fn, false) :
+                (window.attachEvent) ?
+                this.attachEvent('on' + evt, fn) :
+                this['on' + evt] = fn;
+        },
+        _has = function(obj, key) {
+            return Object.prototype.hasOwnProperty.call(obj, key);
+        };
 
-	  function loadImage (el, fn) {
-	    var img = new Image()
-	      , src = el.getAttribute('data-src');
-	    img.onload = function() {
-	      if (!! el.parent)
-	        el.parent.replaceChild(img, el)
-	      else
-	        el.src = src;
+    function loadImage(el, fn) {
+        var img = new Image(),
+            src = el.getAttribute('data-src');
+        img.onload = function() {
+            if (!!el.parent)
+                el.parent.replaceChild(img, el)
+            else
+                el.src = src;
 
-	      fn? fn() : null;
-	    }
-	    img.src = src;
-	  }
+            fn ? fn() : null;
+        }
+        img.src = src;
+    }
 
-	  function elementInViewport(el) {
-	    var rect = el.getBoundingClientRect()
+    function elementInViewport(el) {
+        var rect = el.getBoundingClientRect()
 
-	    return (
-	       rect.top    >= 0
-	    && rect.left   >= 0
-	    && rect.top <= (window.innerHeight || document.documentElement.clientHeight)
-	    )
-	  }
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.top <= (window.innerHeight || document.documentElement.clientHeight)
+        )
+    }
 
-	    var images = new Array()
-	      , query = $q('img.lazy')
-	      , processScroll = function(){
-	          for (var i = 0; i < images.length; i++) {
-	            if (elementInViewport(images[i])) {
-	              loadImage(images[i], function () {
-	                images.splice(i, i);
-	              });
-	            }
-	          };
-	        }
-	      ;
-	    // Array.prototype.slice.call is not callable under our lovely IE8
-	    for (var i = 0; i < query.length; i++) {
-	      images.push(query[i]);
-	    };
+    var images = new Array(),
+        query = $q('img.lazy'),
+        processScroll = function() {
+            for (var i = 0; i < images.length; i++) {
+                if (elementInViewport(images[i])) {
+                    loadImage(images[i], function() {
+                        images.splice(i, i);
+                    });
+                }
+            };
+        };
+    // Array.prototype.slice.call is not callable under our lovely IE8
+    for (var i = 0; i < query.length; i++) {
+        images.push(query[i]);
+    };
 
-	    processScroll();
-	    addEventListener('scroll',processScroll);
+    processScroll();
+    addEventListener('scroll', processScroll);
 
-	  });
+});
 
 // Get the modal
 var modal = document.getElementById("myModal");
 
 function openModal() {
-  document.getElementById("myModal").style.display = "block";
+    document.getElementById("myModal").style.display = "block";
 }
 
 function closeModal() {
-  document.getElementById("myModal").style.display = "none";
+    document.getElementById("myModal").style.display = "none";
 }
 
 function closeOutModal(event) {
-  if (event.target == myModal) {
-    document.getElementById("myModal").style.display = "none";
+    if (event.target == myModal) {
+        document.getElementById("myModal").style.display = "none";
     } else if (event.target == dropdown - content) {
-    document.getElementById(0).classList.toggle("show");
-  }
+        document.getElementById(0).classList.toggle("show");
+    }
 }
 
 var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides((slideIndex += n));
+    showSlides((slideIndex += n));
 }
 
 function currentSlide(n) {
-  showSlides((slideIndex = n));
+    showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-  captionText.innerHTML = dots[slideIndex - 1].alt;
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
 }
 
 /* When the user clicks on the button,
         toggle between hiding and showing the dropdown content */
 function myFunction(i) {
-  document.getElementById(i).classList.toggle("show");
+    document.getElementById(i).classList.toggle("show");
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(e) {
-  if (!e.target.matches(".dropbtn")) {
-    var myDropdown = document.getElementById("myDropdown");
-    if (myDropdown.classList.contains("show")) {
-      myDropdown.classList.remove("show");
+    if (!e.target.matches(".dropbtn")) {
+        var myDropdown = document.getElementById("myDropdown");
+        if (myDropdown.classList.contains("show")) {
+            myDropdown.classList.remove("show");
+        }
     }
-  }
 };
