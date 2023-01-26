1e8f3d20b04c6de84f0326ec4c9af5f6a098b85e
JuriKnauth <knauth.juri@gmail.com>
2023-01-26 13:22:47 +0100

    Formatting, html fixups

--- cross-domain/respond.proxy.js
+++ cross-domain/respond.proxy.js
@@ -1,129 +1,134 @@
 /*! Respond.js: min/max-width media query polyfill. Remote proxy (c) Scott Jehl. MIT/GPLv2 Lic. j.mp/respondjs  */
-(function(win, doc, undefined){
-	var docElem			= doc.documentElement,
-		proxyURL		= doc.getElementById("respond-proxy").href,
-		redirectURL		= (doc.getElementById("respond-redirect") || location).href,
-		baseElem		= doc.getElementsByTagName("base")[0],
-		urls			= [],
-		refNode;
+(function (win, doc, undefined) {
+  var docElem = doc.documentElement,
+    proxyURL = doc.getElementById("respond-proxy").href,
+    redirectURL = (doc.getElementById("respond-redirect") || location).href,
+    baseElem = doc.getElementsByTagName("base")[0],
+    urls = [],
+    refNode;
 
-	function encode(url){
-		return win.encodeURIComponent(url);
-	}
+  function encode(url) {
+    return win.encodeURIComponent(url);
+  }
 
-	 function fakejax( url, callback ){
+  function fakejax(url, callback) {
+    var iframe, AXO;
 
-		var iframe,
-			AXO;
-		
-		// All hail Google https://j.mp/iKMI19
-		// Behold, an iframe proxy without annoying clicky noises.
-		if ( "ActiveXObject" in win ) {
-			AXO = new ActiveXObject( "htmlfile" );
-			AXO.open();
-			AXO.write( '<iframe loading="lazy" id="x"></iframe>' );
-			AXO.close();
-			iframe = AXO.getElementById( "x" );
-		} else {
-			iframe = doc.createElement( "iframe" );
-			iframe.style.cssText = "position:absolute;top:-99em";
-			docElem.insertBefore(iframe, docElem.firstElementChild || docElem.firstChild );
-		}
+    // All hail Google https://j.mp/iKMI19
+    // Behold, an iframe proxy without annoying clicky noises.
+    if ("ActiveXObject" in win) {
+      AXO = new ActiveXObject("htmlfile");
+      AXO.open();
+      AXO.write('<iframe loading="lazy" id="x"></iframe>');
+      AXO.close();
+      iframe = AXO.getElementById("x");
+    } else {
+      iframe = doc.createElement("iframe");
+      iframe.style.cssText = "position:absolute;top:-99em";
+      docElem.insertBefore(
+        iframe,
+        docElem.firstElementChild || docElem.firstChild
+      );
+    }
 
-		iframe.src = checkBaseURL(proxyURL) + "?url=" + encode(redirectURL) + "&css=" + encode(checkBaseURL(url));
-		
-		function checkFrameName() {
-			var cssText;
+    iframe.src =
+      checkBaseURL(proxyURL) +
+      "?url=" +
+      encode(redirectURL) +
+      "&css=" +
+      encode(checkBaseURL(url));
 
-			try {
-				cssText = iframe.contentWindow.name;
-			}
-			catch (e) { }
+    function checkFrameName() {
+      var cssText;
 
-			if (cssText) {
-				// We've got what we need. Stop the iframe from loading further content.
-				iframe.src = "about:blank";
-				iframe.parentNode.removeChild(iframe);
-				iframe = null;
+      try {
+        cssText = iframe.contentWindow.name;
+      } catch (e) {}
 
-			
-				// Per https://j.mp/kn9EPh, not taking any chances. Flushing the ActiveXObject
-				if (AXO) {
-					AXO = null;
+      if (cssText) {
+        // We've got what we need. Stop the iframe from loading further content.
+        iframe.src = "about:blank";
+        iframe.parentNode.removeChild(iframe);
+        iframe = null;
 
-					if (win.CollectGarbage) {
-						win.CollectGarbage();
-					}
-				}
+        // Per https://j.mp/kn9EPh, not taking any chances. Flushing the ActiveXObject
+        if (AXO) {
+          AXO = null;
 
-				callback(cssText);
-			}
-			else{
-				win.setTimeout(checkFrameName, 100);
-			}
-		}
-		
-		win.setTimeout(checkFrameName, 500);
-	}
+          if (win.CollectGarbage) {
+            win.CollectGarbage();
+          }
+        }
 
-    // https://stackoverflow.com/a/472729
-	function checkBaseURL(href) {
-        var el = document.createElement('div'),
-        escapedURL = href.split('&').join('&amp;').
-            split('<').join('&lt;').
-            split('"').join('&quot;');
+        callback(cssText);
+      } else {
+        win.setTimeout(checkFrameName, 100);
+      }
+    }
 
-        el.innerHTML = '<a href="' + escapedURL + '">x</a>';
-        return el.firstChild.href;
-	}
-	
-	function checkRedirectURL() {
-		// IE6 & IE7 don't build out absolute urls in <link /> attributes.
-		// So respond.proxy.gif remains relative instead of https://example.com/respond.proxy.gif.
-		// This trickery resolves that issue.
-		if (~ !redirectURL.indexOf(location.host)) {
+    win.setTimeout(checkFrameName, 500);
+  }
 
-			var fakeLink = doc.createElement("div");
+  // https://stackoverflow.com/a/472729
+  function checkBaseURL(href) {
+    var el = document.createElement("div"),
+      escapedURL = href
+        .split("&")
+        .join("&amp;")
+        .split("<")
+        .join("&lt;")
+        .split('"')
+        .join("&quot;");
 
-			fakeLink.innerHTML = '<a href="' + redirectURL + '"></a>';
-			docElem.insertBefore(fakeLink, docElem.firstElementChild || docElem.firstChild );
+    el.innerHTML = '<a href="' + escapedURL + '">x</a>';
+    return el.firstChild.href;
+  }
 
-			// Grab the parsed URL from that dummy object
-			redirectURL = fakeLink.firstChild.href;
+  function checkRedirectURL() {
+    // IE6 & IE7 don't build out absolute urls in <link /> attributes.
+    // So respond.proxy.gif remains relative instead of https://example.com/respond.proxy.gif.
+    // This trickery resolves that issue.
+    if (~!redirectURL.indexOf(location.host)) {
+      var fakeLink = doc.createElement("div");
 
-			// Clean up
-			fakeLink.parentNode.removeChild(fakeLink);
-			fakeLink = null;
-		}
-	}
-	
-	function buildUrls(){
-		var links = doc.getElementsByTagName( "link" );
-		
-		for( var i = 0, linkl = links.length; i < linkl; i++ ){
-			
-			var thislink	= links[i],
-				href		= links[i].href,
-				extreg		= (/^([a-zA-Z:]*\/\/(www\.)?)/).test( href ),
-				ext			= (baseElem && !extreg) || extreg;
+      fakeLink.innerHTML = '<a href="' + redirectURL + '"></a>';
+      docElem.insertBefore(
+        fakeLink,
+        docElem.firstElementChild || docElem.firstChild
+      );
 
-			//make sure it's an external stylesheet
-			if( thislink.rel.indexOf( "stylesheet" ) >= 0 && ext ){
-				(function( link ){			
-					fakejax( href, function( css ){
-						link.styleSheet.rawCssText = css;
-						respond.update();
-					} );
-				})( thislink );
-			}	
-		}
+      // Grab the parsed URL from that dummy object
+      redirectURL = fakeLink.firstChild.href;
 
-		
-	}
-	
-	if( !respond.mediaQueriesSupported ){
-		checkRedirectURL();
-		buildUrls();
-	}
+      // Clean up
+      fakeLink.parentNode.removeChild(fakeLink);
+      fakeLink = null;
+    }
+  }
 
-})( window, document );
+  function buildUrls() {
+    var links = doc.getElementsByTagName("link");
+
+    for (var i = 0, linkl = links.length; i < linkl; i++) {
+      var thislink = links[i],
+        href = links[i].href,
+        extreg = /^([a-zA-Z:]*\/\/(www\.)?)/.test(href),
+        ext = (baseElem && !extreg) || extreg;
+
+      //make sure it's an external stylesheet
+      if (thislink.rel.indexOf("stylesheet") >= 0 && ext) {
+        (function (link) {
+          fakejax(href, function (css) {
+            link.styleSheet.rawCssText = css;
+            respond.update();
+          });
+        })(thislink);
+      }
+    }
+  }
+
+  if (!respond.mediaQueriesSupported) {
+    checkRedirectURL();
+    buildUrls();
+  }
+})(window, document);
--- jkscript.js
+++ jkscript.js
@@ -1,18 +1,27 @@
 (function (w, d, n, r, t, s) {
-    w.Stomt = w.Stomt || [];
-    t = d.createElement(n);
-    s = d.getElementsByTagName(n)[0];
-    t.async = 1;
-    t.src = r;
-    s.parentNode.insertBefore(t, s)
-})(window, document, 'script', 'https://www.stomt.com/widget.js');
-Stomt.push(['addTab', {
-    appId: 'zJAwvWmlssF2Z8YCTAKehPgAa'
-}]);
-Stomt.push(['addFeed', {
-    appId: 'zJAwvWmlssF2Z8YCTAKehPgAa'
-}]);
-Stomt.push(['addCreate', {
-    appId: 'zJAwvWmlssF2Z8YCTAKehPgAa'
-}]);
-document.addEventListener('touchstart', onTouchStart, {passive: true});
+  w.Stomt = w.Stomt || [];
+  t = d.createElement(n);
+  s = d.getElementsByTagName(n)[0];
+  t.async = 1;
+  t.src = r;
+  s.parentNode.insertBefore(t, s);
+})(window, document, "script", "https://www.stomt.com/widget.js");
+Stomt.push([
+  "addTab",
+  {
+    appId: "zJAwvWmlssF2Z8YCTAKehPgAa",
+  },
+]);
+Stomt.push([
+  "addFeed",
+  {
+    appId: "zJAwvWmlssF2Z8YCTAKehPgAa",
+  },
+]);
+Stomt.push([
+  "addCreate",
+  {
+    appId: "zJAwvWmlssF2Z8YCTAKehPgAa",
+  },
+]);
+document.addEventListener("touchstart", onTouchStart, { passive: true });
